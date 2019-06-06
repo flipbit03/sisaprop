0b48d3b8642f9a3a15ed94ae084ae4dbbef8cacf
Carlos Eduardo <cadu.coelho@gmail.com>
2019-06-06 10:36:16 -0300

    Atualizado launcher .bat.

--- Gerar Mapas de Apropriacao.bat
+++ Gerar Mapas de Apropriacao.bat
@@ -1,11 +1,1 @@
-@echo off 
-
-py -3 sisaprop.py -m sisaprophelper#autoaprop -r autoapropxlsx
-
-
-echo -
-echo ----
-echo Mapas gerados, pressione enter
-echo ----
-echo -
-pause
+@py -3 sisaprop.py -m sisaprophelper#autoaprop -r autoapropxlsx
